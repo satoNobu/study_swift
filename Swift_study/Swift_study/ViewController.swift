c17932bf46fe9e5f0eecbeeae229d4ad00946aa9
Sato Nobu <nananiku.s.60@gmail.com>
2018-11-10 19:12:51 +0900

    プロジェクト作り直し

---
+++ Swift_study/Swift_study/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_study
+//
+//  Created by 信次　智史 on 2018/11/10.
+//  Copyright © 2018年 stoshi nobutsugu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
